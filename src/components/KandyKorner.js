ad9ec7c84a26bada503a760cb3e68b44ff511de4
heidijane <heidi@heidisprouse.com>
2020-04-21 15:14:38 -0500

    Locations now appear on the DOM, added some basic (ugly) css too

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { LocationProvider } from "./locations/LocationProvider"
+import LocationList from "./locations/LocationList"
+import "./KandyKorner.css"
 
 export default () => (
     <>
         <h2>Kandy Korner</h2>
+
+        <h2>Our Locations</h2>
+        <LocationProvider>
+            <LocationList />
+        </LocationProvider>
     </>
 )
